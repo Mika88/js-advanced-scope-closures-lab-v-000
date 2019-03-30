102338c8b8bfb65edf3b7e1b93914caf449a4a6d
Mika88 <mikaspivak@gmail.com>
2019-03-30 20:35:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
-function produceDrivingRange() {
-  return function() {
-
+function produceDrivingRange(blockRange) {
+  return function(beg, end) {
+    let result = parseInt(end, 10) - parseInt(beg, 10)
+    if ( result <= blockRange) {
+      return `within range by ${result}`;
+    }
+    if ( result > blockRange) {
+      return `${result} blocks out of range`;
+    }
   }
 }
